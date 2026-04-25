5b331ba9096df6a29f5c72cc1db2f1b5a528268c
David <20253ds145@utez.edu.mx>
2026-04-25 11:28:26 -0600

    otro cambio

--- merge/src/main/java/org/example/merge/HelloApplication.java
+++ merge/src/main/java/org/example/merge/HelloApplication.java
@@ -16,6 +16,8 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
         System.out.println("cambio en David ");
+        System.out.println("cambio en David121");
+
 
     }
 }
